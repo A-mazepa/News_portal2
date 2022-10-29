6a99ddd94b9ddcf934f76f25e037948848b24c66
A-mazepa <104897885+A-mazepa@users.noreply.github.com>
2022-10-29 21:59:18 +0300

    -

--- NewsPortal/NewsApp/urls.py
+++ NewsPortal/NewsApp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostsList, PostDetail, NewsCreate, ArticleCreate, ArticleUpdate, NewsUpdate
+from .views import PostsList, PostDetail, NewsCreate, ArticleCreate, ArticleUpdate, NewsUpdate, ArticleDelete, NewsDelete
 
 urlpatterns = [
     path('', PostsList.as_view(), name='post_list'),
@@ -8,5 +8,7 @@ urlpatterns = [
     path('news/create/', NewsCreate.as_view(), name='news_create'),
     path('articles/<int:pk>/edit/', ArticleUpdate.as_view(), name='articles_update'),
     path('news/<int:pk>/edit/', NewsUpdate.as_view(), name='news_update'),
+    path('news/<int:pk>/delete/', NewsDelete.as_view(), name='news_delete'),
+    path('articles/<int:pk>/delete/', ArticleDelete.as_view(), name='articles_delete'),
 
 ]
--- NewsPortal/NewsApp/views.py
+++ NewsPortal/NewsApp/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from .models import Post
 from .forms import PostForm
@@ -63,4 +63,16 @@ class NewsUpdate(UpdateView):
     form_class = PostForm
     model = Post
     template_name = 'post_edit.html'
+
+
+class ArticleDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_list')
+
+
+class NewsDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_list')
 
